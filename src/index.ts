6ee6ff7680c8b67fbbcaed131d7921b688bdcf91
taichunmin <taichunmin@gmail.com>
2026-01-19 17:45:56 +0800

    add get /health

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import 'dotenv/config'
 
 import { Hono } from 'hono'
+import { logger } from 'hono/logger'
+import { prettyJSON } from 'hono/pretty-json'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.use(logger())
+app.use(prettyJSON())
+
+app.get('/health', (c) => {
+  return c.json({ status: 'ok' })
 })
 
 export default app
